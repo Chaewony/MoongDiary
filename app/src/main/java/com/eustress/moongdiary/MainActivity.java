51c11f54a96c1027ee1c7247e2cf352da875abe3
Chaewony <tcd1205@naver.com>
2023-04-04 23:53:17 +0900

    Calendar basic

--- app/src/main/java/com/eustress/moongdiary/MainActivity.java
+++ app/src/main/java/com/eustress/moongdiary/MainActivity.java
@@ -3,12 +3,54 @@ package com.eustress.moongdiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.shrikanthravi.collapsiblecalendarview.data.Day;
+import com.shrikanthravi.collapsiblecalendarview.widget.CollapsibleCalendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CollapsibleCalendar collapsibleCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        collapsibleCalendar = findViewById(R.id.calendarView);
+
+        collapsibleCalendar.setCalendarListener(new CollapsibleCalendar.CalendarListener() {
+            @Override
+            public void onDaySelect() {
+                Day day = collapsibleCalendar.getSelectedDay();
+                Log.i(getClass().getName(), "Selected Day: "
+                        + day.getYear() + "/" + (day.getMonth() + 1) + "/" + day.getDay());
+
+                Toast.makeText(MainActivity.this, "Selected Day: "
+                        + day.getYear() + "/" + (day.getMonth() + 1) + "/" + day.getDay(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemClick(View view) {
+
+            }
+
+            @Override
+            public void onDataUpdate() {
+
+            }
+
+            @Override
+            public void onMonthChange() {
+
+            }
+
+            @Override
+            public void onWeekChange(int i) {
+
+            }
+        });
     }
 }
