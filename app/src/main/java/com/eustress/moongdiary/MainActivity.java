4f68740b1e2b667a8968d08bd228b0221567051d
Chaewony <tcd1205@naver.com>
2023-04-08 03:06:02 +0900

    Calendar TopBar
    캘린터 탑 바 없앤 버전 라이브러리 업데이트
    캘린더 today 모양
    탑 바 커스텀

--- app/src/main/java/com/eustress/moongdiary/MainActivity.java
+++ app/src/main/java/com/eustress/moongdiary/MainActivity.java
@@ -1,13 +1,17 @@
 package com.eustress.moongdiary;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -20,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     private Button plusCloudBtn;
     private ImageView moongGif;
 
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private TextView toobarText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +36,20 @@ public class MainActivity extends AppCompatActivity {
         collapsibleCalendar = findViewById(R.id.calendarView);
         plusCloudBtn = findViewById(R.id.cloudBtn);
         moongGif = findViewById(R.id.moongGif);
+        toolbar = findViewById(R.id.toolbar);
+        toobarText = findViewById(R.id.toolbarText);
 
+        //움직이는 뭉티, gif 재생
         Glide.with(this).load(R.drawable.gif_moong).into(moongGif);
-
-        collapsibleCalendar.addEventTag(2023, 3, 7);
+        //달력 위 이벤트 표시 예제 코드
+        collapsibleCalendar.addEventTag(2023, 3, 7, Color.RED);
+        //툴바 관련 세팅
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        UpdateToolbar();
 
         collapsibleCalendar.setCalendarListener(new CollapsibleCalendar.CalendarListener() {
             @Override
@@ -83,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onMonthChange() {
-
+                UpdateToolbar();
             }
 
             @Override
@@ -101,5 +119,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    void UpdateToolbar()
+    {
+        toobarText.setText(collapsibleCalendar.getYear() + "년 " + (collapsibleCalendar.getMonth()+1) +"월");
     }
 }
